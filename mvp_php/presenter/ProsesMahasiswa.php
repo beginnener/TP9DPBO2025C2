8f7a1339abb685e0bad94a2c860987ceb7425a11
nenematoda <natashadind@upi.edu>
2025-06-02 01:15:54 +0700

    add create update delete method for presenter process from view

--- mvp_php/presenter/ProsesMahasiswa.php
+++ mvp_php/presenter/ProsesMahasiswa.php
@@ -26,7 +26,7 @@ class ProsesMahasiswa implements KontrakPresenter
 		}
 	}
 
-	function prosesDataMahasiswa()
+	function prosesDataMahasiswa() // untuk menangani READ
 	{
 		try {
 			// mengambil data di tabel Mahasiswa
@@ -42,6 +42,8 @@ class ProsesMahasiswa implements KontrakPresenter
 				$mahasiswa->setTempat($row['tempat']); // mengisi tempat
 				$mahasiswa->setTl($row['tl']); // mengisi tl
 				$mahasiswa->setGender($row['gender']); // mengisi gender
+				$mahasiswa->setEmail($row['email']); // mengisi gender
+				$mahasiswa->setTelp($row['telp']); // mengisi gender
 
 				$this->data[] = $mahasiswa; // tambahkan data mahasiswa ke dalam list
 			}
@@ -52,6 +54,64 @@ class ProsesMahasiswa implements KontrakPresenter
 			echo "yah error part 2" . $e->getMessage();
 		}
 	}
+
+	function createDataMahasiswa($post){ // fungsi untuk menangani CREATE
+		try{
+			// ambil data dari form
+			$nama = $post['nama'];
+			$nim = $post['nim'];
+			$tempat = $post['tempat'];
+			$tl = $post['tl'];
+			$gender = $post['gender'];
+			$email = $post['email'];
+			$telp = $post['telp'];
+			// panggil fungsi createMahasiswa dari TabelMahasiswa
+			$this->tabelmahasiswa->open();
+			$this->tabelmahasiswa->createMahasiswa($nama, $nim, $tempat, $tl, $gender, $email, $telp);
+			
+			// Tutup koneksi
+			$this->tabelmahasiswa->close();
+		} catch (Exception $e) {
+			// memproses error
+			echo "gagal input data" . $e->getMessage();
+		}
+	}
+
+	function updateDataMahasiswa($post){
+		try{
+			// ambil data dari form
+			$nama = $post['nama'];
+			$nim = $post['nim'];
+			$tempat = $post['tempat'];
+			$tl = $post['tl'];
+			$gender = $post['gender'];
+			$email = $post['email'];
+			$telp = $post['telp'];
+			// panggil fungsi updateMahasiswa dari TabelMahasiswa
+			$this->tabelmahasiswa->open();
+			$this->tabelmahasiswa->updateMahasiswa($nama, $nim, $tempat, $tl, $gender, $email, $telp);
+			// Tutup koneksi
+			$this->tabelmahasiswa->close();
+		} catch (Exception $e) {
+			// memproses error
+			echo "gagal update" . $e->getMessage();
+		}
+	}
+
+	function deleteDataMahasiswa($nim)
+	{
+		try{
+			// panggil fungsi deleteMahasiswa dari TabelMahasiswa
+			$this->tabelmahasiswa->open();
+			$this->tabelmahasiswa->deleteMahasiswa($nim);
+			// Tutup koneksi
+			$this->tabelmahasiswa->close();
+		} catch (Exception $e) {
+			// memproses error
+			echo "gagal delete" . $e->getMessage();
+		}
+	}
+
 	function getId($i)
 	{
 		// mengembalikan id mahasiswa dengan indeks ke i
@@ -81,6 +141,16 @@ class ProsesMahasiswa implements KontrakPresenter
 	{
 		// mengembalikan gender mahasiswa dengan indeks ke i
 		return $this->data[$i]->gender;
+	}
+	function getEmail($i)
+	{
+		// mengembalikan email mahasiswa dengan indeks ke i
+		return $this->data[$i]->email;
+	}
+	function getTelp($i)
+	{
+		// mengembalikan telp mahasiswa dengan indeks ke i
+		return $this->data[$i]->telp;
 	}
 	function getSize()
 	{
