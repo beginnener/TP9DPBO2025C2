16874a4b13031582339064aa6ee3e008332d30d4
nenematoda <natashadind@upi.edu>
2025-06-02 01:15:26 +0700

    add create, update, and delete method for model communicating with database

--- mvp_php/model/TabelMahasiswa.class.php
+++ mvp_php/model/TabelMahasiswa.class.php
@@ -7,7 +7,7 @@
 // Kelas yang berisikan tabel dari mahasiswa
 class TabelMahasiswa extends DB
 {
-	function getMahasiswa()
+	function getMahasiswa() // untuk READ
 	{
 		// Query mysql select data mahasiswa
 		$query = "SELECT * FROM mahasiswa";
@@ -15,4 +15,33 @@ class TabelMahasiswa extends DB
 		// Mengeksekusi query
 		return $this->execute($query);
 	}
+
+	function createMahasiswa($nama, $nim, $tempat, $tl, $gender, $email, $telp) // untuk CREATE
+	{
+		// Query mysql insert data mahasiswa
+		$query = "INSERT INTO mahasiswa (nama, nim, tempat, tl, gender, email, telp)
+				  VALUES ('$nama', '$nim', '$tempat', '$tl', '$gender', '$email', '$telp')";
+
+		// Mengeksekusi query
+		return $this->execute($query);
+	}
+
+	function updateMahasiswa($nama, $nim, $tempat, $tl, $gender, $email, $telp) // untuk UPDATE
+	{
+		// Query mysql update data mahasiswa
+		$query = "UPDATE mahasiswa 
+				  SET nama='$nama', nim='$nim', tempat='$tempat', tl='$tl', gender='$gender', email='$email', telp='$telp' 
+				  WHERE nim='$nim'";
+		// Mengeksekusi query
+		return $this->execute($query);
+	}
+
+	function deleteMahasiswa($nim) // untuk DELETE
+	{
+		// Query mysql delete data mahasiswa
+		$query = "DELETE FROM mahasiswa WHERE nim='$nim'";
+		
+		// Mengeksekusi query
+		return $this->execute($query);
+	}	
 }
